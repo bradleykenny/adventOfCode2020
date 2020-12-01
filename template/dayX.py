88a1f8c54b37c17ba3bf43ffdd6a1198cd71d475
bradleykenny <bkenny283@gmail.com>
2020-12-01 19:42:17 +1100

    Added template.

---
+++ template/dayX.py
@@ -0,0 +1,16 @@
+import math
+
+print("========== PART 1 ==========")
+
+inFile = open("day1.txt")
+
+arr = [int(line) for line in inFile]
+n = len(arr)
+
+for line in inFile:
+	print(line)
+
+print("\n========== PART 2 ==========")
+
+for line in inFile:
+	print(line)
